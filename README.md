36a2a3d0c9507aea8496ac381ee87a8af88fb366
vagrant <vagrant@aws-box>
2024-09-14 09:13:06 +0000

    vault-cluster-code-added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # co7-project
+
+## Create following ENV Variable on Terraform WrokSpace
+HCP_CLIENT_ID="..."
+HCP_CLIENT_SECRET="..."
---
+++ vault-cluster/main.tf
@@ -0,0 +1,13 @@
+resource "hcp_hvn" "vault_hvn" {
+  hvn_id         = "co7-hvn"
+  cloud_provider = "aws"
+  region         = "ap-southeast-1"
+  cidr_block     = "172.25.16.0/20"
+}
+
+resource "hcp_vault_cluster" "vault_cluster" {
+  cluster_id = "co7-vault-cluster"
+  hvn_id     = hcp_hvn.vault_hvn.hvn_id
+  tier       = "dev"
+  public_endpoint = true
+}
