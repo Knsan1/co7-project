b777d20fc7ffe2873c975e8f1e38be84d6539246
vagrant <vagrant@aws-box>
2024-09-15 16:45:15 +0000

    modify configure jwt auth code

--- configure-jwt-auth-on-vault/main.tf
+++ configure-jwt-auth-on-vault/main.tf
@@ -64,7 +64,7 @@ EOT
 
 resource "vault_jwt_auth_backend_role" "admin_role" {
   backend = vault_jwt_auth_backend.example.path
-  role_name = admin-role
+  role_name = "admin-role"
   token_policies = [vault_policy.example.name]
   bound_audiences = ["vault.workload.identity"]
   bound_claims_type = "glob"
